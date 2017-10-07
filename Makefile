d9333d9a978e9d734d9bb21059029bbfa570fa55
aliaspider <aliaspider@gmail.com>
2017-10-07 10:04:40 +0100

    misc

---
+++ Makefile
@@ -0,0 +1,127 @@
+
+TARGET = snes9x_module.a
+DEBUG = 0
+
+platform = linux
+
+BUILD_DIR = objs/$(platform)
+
+ifeq ($(DEBUG),1)
+   BUILD_DIR := $(BUILD_DIR)-dbg
+endif
+
+all: $(TARGET)
+
+OBJS :=
+OBJS += interface.o
+OBJS += apu/apu.o
+OBJS += apu/bapu/dsp/sdsp.o
+OBJS += apu/bapu/dsp/SPC_DSP.o
+OBJS += apu/bapu/smp/smp.o
+OBJS += apu/bapu/smp/smp_state.o
+OBJS += bsx.o
+OBJS += c4.o
+OBJS += c4emu.o
+OBJS += cheats.o
+OBJS += cheats2.o
+OBJS += clip.o
+OBJS += conffile.o
+OBJS += controls.o
+OBJS += cpu.o
+OBJS += cpuexec.o
+OBJS += cpuops.o
+OBJS += crosshairs.o
+OBJS += dma.o
+OBJS += dsp.o
+OBJS += dsp1.o
+OBJS += dsp2.o
+OBJS += dsp3.o
+OBJS += dsp4.o
+OBJS += fxinst.o
+OBJS += fxemu.o
+OBJS += gfx.o
+OBJS += globals.o
+OBJS += logger.o
+OBJS += memmap.o
+OBJS += msu1.o
+OBJS += movie.o
+OBJS += obc1.o
+OBJS += ppu.o
+OBJS += stream.o
+OBJS += sa1.o
+OBJS += sa1cpu.o
+OBJS += screenshot.o
+OBJS += sdd1.o
+OBJS += sdd1emu.o
+OBJS += seta.o
+OBJS += seta010.o
+OBJS += seta011.o
+OBJS += seta018.o
+OBJS += snapshot.o
+OBJS += snes9x.o
+OBJS += spc7110.o
+OBJS += srtc.o
+OBJS += tile.o
+OBJS += filter/2xsai.o
+OBJS += filter/blit.o
+OBJS += filter/epx.o
+OBJS += filter/hq2x.o
+OBJS += filter/snes_ntsc.o
+OBJS += statemanager.o
+
+OBJS := $(addprefix $(BUILD_DIR)/,$(OBJS))
+
+ifeq ($(DEBUG),1)
+   FLAGS += -g -O0
+else
+   FLAGS += -O3
+endif
+
+FLAGS += -Wall -Wextra -Werror
+FLAGS += -Wno-sign-compare
+FLAGS += -Wno-misleading-indentation
+FLAGS += -Wno-unused-parameter
+FLAGS += -Wno-unused-variable
+FLAGS += -Wno-unused-value
+FLAGS += -Wno-unused-function
+FLAGS += -Wno-unused-but-set-variable
+FLAGS += -Wno-int-to-pointer-cast
+FLAGS += -Werror=implicit-function-declaration -Werror=incompatible-pointer-types
+FLAGS += -Wno-array-bounds
+FLAGS += -fomit-frame-pointer
+
+#FLAGS += -DHAVE_STRINGS_H
+FLAGS += -DHAVE_STDINT_H
+#FLAGS += -DPTR_NOT_INT
+FLAGS += -DRIGHTSHIFT_IS_SAR
+
+FLAGS += -I. -Iapu/ -Iapu/bapu -Ijma/ -Ifilter/
+
+CFLAGS += $(FLAGS)
+CXXFLAGS += $(FLAGS) -fno-exceptions -fno-rtti
+
+
+$(BUILD_DIR)/$(TARGET): $(OBJS) .lastbuild
+	touch .lastbuild
+	$(AR) rcs $@ $(OBJS)
+
+$(TARGET): $(BUILD_DIR)/$(TARGET)
+	cp $< $@
+
+$(BUILD_DIR)/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $< $(CFLAGS) -MT $@ -MMD -MP -MF $(BUILD_DIR)/$*.depend -c -o $@
+
+$(BUILD_DIR)/%.o: %.cpp
+	@mkdir -p $(dir $@)
+	$(CC) $< $(CXXFLAGS) -MT $@ -MMD -MP -MF $(BUILD_DIR)/$*.depend -c -o $@
+
+.lastbuild: ;
+
+clean:
+#	rm -rf objs
+	rm -f $(OBJS) $(OBJS:.o=.depend)
+	rm -f $(BUILD_DIR)/$(TARGET) $(TARGET) .lastbuild
+
+
+-include $(OBJS:.o=.depend)
---
+++ interface.cpp
@@ -0,0 +1,355 @@
+
+#include <sys/stat.h>
+
+#include "../interface.h"
+#include "snes9x.h"
+#include "memmap.h"
+#include "display.h"
+#include "controls.h"
+#include "apu.h"
+#include "cpuexec.h"
+
+void module_init(const module_init_info_t *init_info, module_info_t *module_info)
+{
+   memset(&Settings, 0, sizeof(Settings));
+	Settings.MouseMaster = TRUE;
+	Settings.SuperScopeMaster = TRUE;
+	Settings.JustifierMaster = TRUE;
+	Settings.MultiPlayer5Master = TRUE;
+	Settings.FrameTimePAL = 20000;
+	Settings.FrameTimeNTSC = 16667;
+	Settings.SixteenBitSound = TRUE;
+	Settings.Stereo = TRUE;
+	Settings.SoundPlaybackRate = 32000;
+	Settings.SoundInputRate = 32000;
+	Settings.SupportHiRes = TRUE;
+	Settings.Transparency = TRUE;
+	Settings.AutoDisplayMessages = TRUE;
+	Settings.InitialInfoStringTimeout = 120;
+	Settings.HDMATimingHack = 100;
+	Settings.BlockInvalidVRAMAccessMaster = TRUE;
+	Settings.StopEmulation = TRUE;
+	Settings.WrongMovieStateProtection = TRUE;
+	Settings.DumpStreamsMaxFrames = -1;
+	Settings.StretchScreenshots = 1;
+	Settings.SnapshotScreenshots = TRUE;
+	Settings.SkipFrames = AUTO_FRAMERATE;
+	Settings.TurboSkipFrames = 15;
+	Settings.CartAName[0] = 0;
+	Settings.CartBName[0] = 0;
+
+   CPU.Flags = 0;
+
+   Memory.Init();
+   GFX.Pitch = 512;
+   S9xGraphicsInit();
+   S9xSetRenderPixelFormat(RGB565);
+   S9xInitAPU();
+   S9xInitSound(20, 20);
+
+   if (!Memory.LoadROM(init_info->filename))
+      debug_log("load error\n");
+
+   module_info->output_width = 256;
+   module_info->output_height = 224;
+   module_info->screen_format = screen_format_RGB565;
+   module_info->stereo = true;
+   module_info->framerate = 60;
+   module_info->audio_samples_per_frame = 44100.0 / module_info->framerate;
+
+   debug_log("module init\n");
+}
+
+void module_destroy()
+{
+   S9xGraphicsDeinit();
+   debug_log("module destroy\n");
+}
+
+bool8 S9xDeinitUpdate (int width, int height)
+{
+   return TRUE;
+}
+
+void module_run(module_run_info_t *run_info)
+{
+   GFX.Screen = run_info->screen.u16;
+//   GFX.Pitch = run_info->pitch;
+//   GFX.ScreenSize = GFX.Pitch / 2 * SNES_HEIGHT_EXTENDED * (Settings.SupportHiRes ? 2 : 1);
+//   GFX.PPL = GFX.RealPPL = GFX.Pitch >> 1;
+
+   S9xMainLoop();
+
+   run_info->frame_completed = true;
+}
+
+void _splitpath (const char *path, char *drive, char *dir, char *fname, char *ext)
+{
+	*drive = 0;
+
+	const char	*slash = strrchr(path, SLASH_CHAR),
+				*dot   = strrchr(path, '.');
+
+	if (dot && slash && dot < slash)
+		dot = NULL;
+
+	if (!slash)
+	{
+		*dir = 0;
+
+		strcpy(fname, path);
+
+		if (dot)
+		{
+			fname[dot - path] = 0;
+			strcpy(ext, dot + 1);
+		}
+		else
+			*ext = 0;
+	}
+	else
+	{
+		strcpy(dir, path);
+		dir[slash - path] = 0;
+
+		strcpy(fname, slash + 1);
+
+		if (dot)
+		{
+			fname[dot - slash - 1] = 0;
+			strcpy(ext, dot + 1);
+		}
+		else
+			*ext = 0;
+	}
+}
+
+void _makepath (char *path, const char *, const char *dir, const char *fname, const char *ext)
+{
+	if (dir && *dir)
+	{
+		strcpy(path, dir);
+		strcat(path, SLASH_STR);
+	}
+	else
+		*path = 0;
+
+	strcat(path, fname);
+
+	if (ext && *ext)
+	{
+		strcat(path, ".");
+		strcat(path, ext);
+	}
+}
+void S9xMessage (int type, int number, const char *message)
+{
+	const int	max = 36 * 3;
+	static char	buffer[max + 1];
+
+	debug_log("%s\n", message);
+	strncpy(buffer, message, max + 1);
+	buffer[max] = 0;
+	S9xSetInfoString(buffer);
+}
+
+bool S9xPollButton (uint32 id, bool *pressed)
+{
+	return 0;
+}
+bool8 S9xInitUpdate (void)
+{
+	return TRUE;
+}
+
+bool8 S9xContinueUpdate (int width, int height)
+{
+    return TRUE;
+}
+
+void S9xSetPalette (void)
+{
+}
+
+void S9xAutoSaveSRAM (void)
+{
+	Memory.SaveSRAM(S9xGetFilename(".srm", SRAM_DIR));
+}
+
+static const char	dirNames[13][32] =
+{
+	"",				// DEFAULT_DIR
+	"",				// HOME_DIR
+	"",				// ROMFILENAME_DIR
+	"rom",			// ROM_DIR
+	"sram",			// SRAM_DIR
+	"savestate",	// SNAPSHOT_DIR
+	"screenshot",	// SCREENSHOT_DIR
+	"spc",			// SPC_DIR
+	"cheat",		// CHEAT_DIR
+	"patch",		// PATCH_DIR
+	"bios",			// BIOS_DIR
+	"log",			// LOG_DIR
+	""
+};
+
+const char* s9x_base_dir = ".";
+
+const char * S9xGetDirectory (enum s9x_getdirtype dirtype)
+{
+	static char	s[PATH_MAX + 1];
+
+	if (dirNames[dirtype][0])
+		snprintf(s, PATH_MAX + 1, "%s%s%s", s9x_base_dir, SLASH_STR, dirNames[dirtype]);
+	else
+	{
+		switch (dirtype)
+		{
+			case DEFAULT_DIR:
+				strncpy(s, s9x_base_dir, PATH_MAX + 1);
+				s[PATH_MAX] = 0;
+				break;
+
+			case HOME_DIR:
+				strncpy(s, getenv("HOME"), PATH_MAX + 1);
+				s[PATH_MAX] = 0;
+				break;
+
+			case ROMFILENAME_DIR:
+				strncpy(s, Memory.ROMFilename, PATH_MAX + 1);
+				s[PATH_MAX] = 0;
+
+				for (int i = strlen(s); i >= 0; i--)
+				{
+					if (s[i] == SLASH_CHAR)
+					{
+						s[i] = 0;
+						break;
+					}
+				}
+
+				break;
+
+			default:
+				s[0] = 0;
+				break;
+		}
+	}
+
+	return (s);
+}
+
+
+const char * S9xGetFilename (const char *ex, enum s9x_getdirtype dirtype)
+{
+	static char	s[PATH_MAX + 1];
+	char		drive[_MAX_DRIVE + 1], dir[_MAX_DIR + 1], fname[_MAX_FNAME + 1], ext[_MAX_EXT + 1];
+
+	_splitpath(Memory.ROMFilename, drive, dir, fname, ext);
+	snprintf(s, PATH_MAX + 1, "%s%s%s%s", S9xGetDirectory(dirtype), SLASH_STR, fname, ex);
+
+	return (s);
+}
+
+bool8 S9xOpenSnapshotFile (const char *filename, bool8 read_only, STREAM *file)
+{
+	char	s[PATH_MAX + 1];
+	char	drive[_MAX_DRIVE + 1], dir[_MAX_DIR + 1], fname[_MAX_FNAME + 1], ext[_MAX_EXT + 1];
+
+	_splitpath(filename, drive, dir, fname, ext);
+
+	if (*drive || *dir == SLASH_CHAR || (strlen(dir) > 1 && *dir == '.' && *(dir + 1) == SLASH_CHAR))
+	{
+		strncpy(s, filename, PATH_MAX + 1);
+		s[PATH_MAX] = 0;
+	}
+	else
+		snprintf(s, PATH_MAX + 1, "%s%s%s", S9xGetDirectory(SNAPSHOT_DIR), SLASH_STR, fname);
+
+	if (!*ext && strlen(s) <= PATH_MAX - 4)
+		strcat(s, ".frz");
+
+	if ((*file = OPEN_STREAM(s, read_only ? "rb" : "wb")))
+		return (TRUE);
+
+	return (FALSE);
+}
+
+void S9xCloseSnapshotFile (STREAM file)
+{
+	CLOSE_STREAM(file);
+}
+
+const char * S9xBasename (const char *f)
+{
+	const char	*p;
+
+	if ((p = strrchr(f, '/')) != NULL || (p = strrchr(f, '\\')) != NULL)
+		return (p + 1);
+
+	return (f);
+}
+
+bool8 S9xOpenSoundDevice (void)
+{
+   return TRUE;
+}
+
+const char * S9xGetFilenameInc (const char *ex, enum s9x_getdirtype dirtype)
+{
+	static char	s[PATH_MAX + 1];
+	char		drive[_MAX_DRIVE + 1], dir[_MAX_DIR + 1], fname[_MAX_FNAME + 1], ext[_MAX_EXT + 1];
+
+	unsigned int	i = 0;
+	const char		*d;
+	struct stat		buf;
+
+	_splitpath(Memory.ROMFilename, drive, dir, fname, ext);
+	d = S9xGetDirectory(dirtype);
+
+	do
+		snprintf(s, PATH_MAX + 1, "%s%s%s.%03d%s", d, SLASH_STR, fname, i++, ex);
+	while (stat(s, &buf) == 0 && i < 1000);
+
+	return (s);
+}
+void S9xExit (void)
+{
+}
+const char * S9xChooseFilename (bool8 read_only)
+{
+	return "";
+}
+
+const char * S9xChooseMovieFilename (bool8 read_only)
+{
+   return "";
+}
+
+void S9xToggleSoundChannel (int c)
+{
+
+}
+const char * S9xStringInput (const char *message)
+{
+	return (NULL);
+}
+
+void S9xHandlePortCommand (s9xcommand_t cmd, int16 data1, int16 data2)
+{
+
+}
+
+bool S9xPollAxis (uint32 id, int16 *value)
+{
+    return true;
+}
+
+bool S9xPollPointer (uint32 id, int16 *x, int16 *y)
+{
+    return true;
+}
+void S9xSyncSpeed (void)
+{
+
+}
